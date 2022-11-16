6c7f49ab1c4daa66122c110b9d00524447ceae14
m1212e <->
2022-11-16 17:20:45 +0100

    Fixed tests

--- __test__/index.spec.mjs
+++ __test__/index.spec.mjs
@@ -6,12 +6,12 @@ test('test bindings', (t) => {
   const server = new ERPCServer({
     port: 9988,
     allowedCorsOrigins: ["*"]
-  }, ["http-server"], true, "Backend");
+  }, "http-server", true, "Backend");
 
   const target = new ERPCTarget({
     address: "",
     port: 0
-  }, ["http-server"])
+  }, "http-server")
 
   t.assert(server != undefined)
   t.assert(target != undefined)
